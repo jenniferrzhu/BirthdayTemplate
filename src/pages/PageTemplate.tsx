487e24af42219ddf0ff889e5d0eb1689a2224dc4
Jennifer <jenniferrzhuu@gmail.com>
2024-07-14 18:01:08 -0700

    Updated Page Styling

--- src/pages/PageTemplate.tsx
+++ src/pages/PageTemplate.tsx
@@ -1,32 +1,49 @@
-import React from 'react'
-import './styles/Album.css'; 
-import pic from '../imgs/flowers.jpg';
+import React from "react";
+import "./styles/Album.css";
+import pic from "../imgs/flowers.jpg";
 
 const PageTemplate = () => {
   return (
-    <div className='page'>
-        <h1>Title</h1>
-        <h3>~subtitle~</h3> 
-        <div className="cards">
-            <div className="card">
-                <img src={pic} />
-                <h3>caption</h3> 
-            </div>
-            <div className="card">
-                <img src={pic} />
-                <h3>caption</h3> 
-            </div>
-            <div className="card">
-                <img src={pic} />
-                <h3>caption</h3> 
-            </div>
+    <div className="page">
+      <h1>Title</h1>
+      <h3>~subtitle~</h3>
+      <div className="cards">
+        <div className="card">
+          <img src={pic} />
+          <h3>caption</h3>
         </div>
-        <p>Insert Message</p> 
-        <a className="back" href="/timeline">
-            return
-        </a>
+        <div className="card">
+          <img src={pic} />
+          <h3>caption</h3>
+        </div>
+        <div className="card">
+          <img src={pic} />
+          <h3>caption</h3>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <a className="back" href="/timeline">
+        return
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default PageTemplate
+export default PageTemplate;
